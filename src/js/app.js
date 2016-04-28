e0d01f03b69180b1e05f43e90892bb049e7e6f5b
xiyuyizhi <1553810509@qq.com>
2016-04-28 14:24:13 +0800

    代码优化整改

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wangwei on 2016/4/28.
+ */
+$(function(){
+    var player=new VideoPlay($('#video'),'1.mp4');
+    player.init();
+});
--- src/js/zjmy.vPlayer.js
+++ src/js/zjmy.vPlayer.js
@@ -2,122 +2,136 @@
  * Created by wangwei on 2016/4/27.
  */
 /***
- * �¼��Ⱥ�˳��:
+ * 执行顺序
  * loadedmetadata
  * loadeddata
  * canplay
  * oncanplaythrough
  */
-$(function () {
-    var timer,
-        store={
-            'duration':0,
-            'currentTime':0
-        },
-        videoPlayer = $('#video'),
-        videoWrap=$('#videoWrap'),
-        height = {
-            'screenHeight': $(window).height(),
-            'videoOriginHeight': 0
+(function (w) {
+    w.VideoPlay = VideoPlay;
+
+    function VideoPlay($obj, videoPath) {
+        this.timer = null;
+        this.store = {
+            'duration': 0,
+            'currentTime': 0
         };
-    videoPlayer.on('canplay', function () {
-        console.log('canplay')
-    });
-    videoPlayer.on('canplaythrough', function () {
-        console.log('oncanplaythrough')
-    })
-    videoPlayer.on('loadeddata', function (data) {
-        console.log('loadeddata');
-        console.log(videoPlayer.get(0).duration);
-        var timeStr;
-        store['duration']=videoPlayer.get(0).duration;
-        timeStr=formateTime(videoPlayer.get(0).duration).join(':');
-        videoWrap.find('.totalTime').html(timeStr);
-    });
-    videoPlayer.on('loadedmetadata', function (data) {
-        console.log('loadedmetadata');
-        console.log(data)
-    });
-    videoPlayer.on('ended', function (e) {
-        console.log('ended');
-        store['currentTime']=0;
-        clearInterval(timer);
-    });
-
-    $('button').click(function () {
-        videoPlayer.addClass('fullScreen')
-    });
-    $(document).on('click', 'button', function (e) {
-        switch (e.target.className) {
-            case 'btn_play':
-                videoPlayer.get(0).play();
-                clearInterval(timer);
-                timer=setInterval(timerFn,1000);
-                break;
-            case 'btn_pause':
-                videoPlayer.get(0).pause();
-                clearInterval(timer);
-                break;
-            case 'btn_fullScreen':
-                if ($(this).text() == '全屏') {
-                    $('#videoWrap').addClass('fullScreen');
-                    videoPlayer.addClass('fullScreen');
-                    $(this).text('还原');
-                } else {
-                    $('#videoWrap').removeClass('fullScreen');
-                    videoPlayer.removeClass('fullScreen');
-                    $(this).text('全屏')
-                }
-                break;
-        }
-    });
-
-    function timerFn(){
-        store.currentTime++;
-        if(store.currentTime>=Math.ceil(store.duration)){
-            clearInterval(timer);
-        }
-        scrollBarFn(videoWrap,store)
+        this.videoPlayer = $obj;
+        this.videoWrap = $obj.parent();
+        this.videoPath = videoPath;
+        this.unit = new Unit();
     }
 
-    //格式化视频时长
-    function formateTime(second){
-        var timeArr=[];
-        if(second>3600){
-            if(second>=3600*10){
-                timeArr.push(Math.floor(second/3600))
-            }else{
-                timeArr.push('0'+Math.floor(second/3600))
-            }
-            timeArr.push('00').push('00');
-        }else if(second>60){
-            timeArr.push('00');
-            if(second>=60*10){
-                timeArr.push(Math.floor(second/60))
-            }else{
-                timeArr.push('0'+Math.floor(second/60))
-            }
-            if((second%60)>10){
-                timeArr.push(Math.ceil(second%60))
-            }else{
-                timeArr.push('0'+Math.ceil(second%60))
-            }
-        }else{
-            timeArr.push('00');
-            timeArr.push('00');
-            timeArr.push(Math.ceil(second));
-        }
-        return timeArr;
+    VideoPlay.prototype.init = function () {
+        this.videoPlayer.attr('src', this.videoPath);
+        this.loaded();
+        this.eventHandle();
+        this.end();
     };
 
-    function scrollBarFn($obj,store){
-        var $scrollPoint=$obj.find('.processBar_scrollPoint'),
-            $processBar_bg=$obj.find('.processBar_bg'),
-            pointWidth=(store.currentTime/store.duration)*$processBar_bg.width();
-        pointWidth=Math.ceil(pointWidth);
-        $scrollPoint.css('width',pointWidth+'px');
-        console.log(pointWidth);
+    VideoPlay.prototype.loaded = function () {
+        var _this = this;
+        this.videoPlayer.on('loadeddata', function (e) {
+            var timeStr;
+            _this.store['duration'] = e.target.duration;
+            timeStr = _this.unit.formateTime(e.target.duration).join(':');
+            _this.videoWrap.find('.totalTime').html(timeStr);
+        })
+    };
+    VideoPlay.prototype.end = function () {
+        var _this = this;
+        this.videoPlayer.on('ended', function (e) {
+            console.log('ended');
+            _this.store['currentTime'] = 0;
+            clearInterval(_this.timer);
+        });
+    };
+    VideoPlay.prototype.eventHandle = function () {
+        var _this = this,
+            $button = this.videoWrap.find('button');
+        $button.on('click', function (e) {
+            switch (e.target.className) {
+                case 'btn_play':
+                    _this.videoPlayer.get(0).play();
+                    clearInterval(_this.timer);
+                    _this.timerFn();
+                    break;
+                case 'btn_pause':
+                    _this.videoPlayer.get(0).pause();
+                    clearInterval(_this.timer);
+                    break;
+                case 'btn_fullScreen':
+                    if ($(this).text() == '全屏') {
+                        _this.videoWrap.addClass('fullScreen');
+                        _this.videoPlayer.addClass('fullScreen');
+                        $(this).text('还原');
+                    }
+                    else {
+                        _this.videoWrap.removeClass('fullScreen');
+                        _this.videoPlayer.removeClass('fullScreen');
+                        $(this).text('全屏')
+                    }
+                    break;
+            }
+        })
+    };
+    VideoPlay.prototype.timerFn = function () {
+        console.log(this);
+        var _this=this;
+        _this.timer=setInterval(function(){
+            _this.store.currentTime++;
+            if (_this.store.currentTime >= Math.ceil(_this.store.duration)) {
+                clearInterval(this.timer);
+            }
+            _this.unit.scrollBarFn(_this.videoWrap, _this.store);
+        },1000);
 
     }
 
-})
+})(window);
+
+
+function Unit() {
+
+}
+
+//格式化视频时长
+Unit.prototype.formateTime = function (second) {
+    var timeArr = [];
+    if (second > 3600) {
+        if (second >= 3600 * 10) {
+            timeArr.push(Math.floor(second / 3600))
+        } else {
+            timeArr.push('0' + Math.floor(second / 3600))
+        }
+        timeArr.push('00').push('00');
+    } else if (second > 60) {
+        timeArr.push('00');
+        if (second >= 60 * 10) {
+            timeArr.push(Math.floor(second / 60))
+        } else {
+            timeArr.push('0' + Math.floor(second / 60))
+        }
+        if ((second % 60) > 10) {
+            timeArr.push(Math.ceil(second % 60))
+        } else {
+            timeArr.push('0' + Math.ceil(second % 60))
+        }
+    } else {
+        timeArr.push('00');
+        timeArr.push('00');
+        timeArr.push(Math.ceil(second));
+    }
+    return timeArr;
+};
+
+//生成进度条
+Unit.prototype.scrollBarFn = function ($obj, store) {
+    var $scrollPoint = $obj.find('.processBar_scrollPoint'),
+        $processBar_bg = $obj.find('.processBar_bg'),
+        pointWidth = (store.currentTime / store.duration) * $processBar_bg.width();
+    pointWidth = Math.ceil(pointWidth);
+    $scrollPoint.css('width', pointWidth + 'px');
+    console.log(pointWidth);
+}
