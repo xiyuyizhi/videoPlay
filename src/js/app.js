eb19510d49d545389a3eb4c356a86fc1a5adddae
xiyuyizhi <1553810509@qq.com>
2016-04-29 18:01:03 +0800

    no message

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@
 $(function(){
     var player=new VideoPlay($('#video'),{
         //'path':'http://192.168.1.79:9000/rest/download/8fd8e560-f22b-4203-8a1f-1fac5686fb2f',
-        'path':'http://v2.mukewang.com/740f398f-adf3-4b0d-9449-732e18222b91/L.mp4?auth_key=1461922906-0-0-3e05de658674272cb0f02b548cf8f988',
+        'path':'http://v2.mukewang.com/d79a0b8f-144d-40f6-ac87-5ac2d1e5d6b7/L.mp4?auth_key=1461925505-0-0-83a19ffb08b9e232dcd43d1b5bea9db4',
         //'path':'L.mp4',
         'autoplay':true
     });
